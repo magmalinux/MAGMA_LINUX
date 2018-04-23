1a7ce256b24ffdb8fd3cbe7ecdaa39236bef7678
LEBRO <37965709+lebrotech@users.noreply.github.com>
2018-04-23 16:19:55 -0600

    Update install.sh

--- programs-install-files/enthusiust-pro-software-selection/install.sh
+++ programs-install-files/enthusiust-pro-software-selection/install.sh
@@ -1,4 +1,4 @@
 echo "run as root to run";sleep 5
-apt install dialog chromium breeze-icon-theme kdenlive unzip openssh-server vim qterminal
+apt install dialog chromium breeze-icon-theme kdenlive unzip openssh-server vim qterminal xorg
 echo "if packages don't work, run apt -f install"
 echo "if everything works YOU ARE DONE!!"
